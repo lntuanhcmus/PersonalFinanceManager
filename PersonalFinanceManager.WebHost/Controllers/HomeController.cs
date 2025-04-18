cf04a9965274ceeccf0555c94a88fb8ac54e0aaa
Tuan Le <lntuanhcmus@gmail.com>
2025-04-18 20:46:12 +0700

    Create the static page like privacy about and contact page

---
+++ PersonalFinanceManager.WebHost/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PersonalFinanceManager.Shared.Dto; // Giả sử DTO nằm trong namespace này
+using PersonalFinanceManager; // Giả sử repository nằm trong namespace này
+using Microsoft.AspNetCore.Authorization;
+
+namespace PersonalFinanceManager.WebHost.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "TransactionsManagement");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
